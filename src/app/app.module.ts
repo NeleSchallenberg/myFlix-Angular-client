04a64b7da566d25ea19c011d93c829b636ac0afd
NeleSchallenberg <nschallenberg@gmx.net>
2023-05-18 19:34:13 +0200

    Install Angular Material and create registration component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,38 @@
 import { NgModule } from '@angular/core';
+
 // Import simplified API to communicate with API or server-side
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Impport Material modules
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { FormsModule } from '@angular/forms';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
-  providers: [],
-  bootstrap: [AppComponent],
+	declarations: [AppComponent, UserRegistrationFormComponent],
+	imports: [
+		BrowserModule,
+		HttpClientModule,
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		FormsModule,
+		MatDialogModule,
+		MatInputModule,
+		MatButtonModule,
+		MatFormFieldModule,
+		MatSnackBarModule,
+		MatCardModule,
+	],
+	providers: [],
+	bootstrap: [AppComponent],
 })
 export class AppModule {}
