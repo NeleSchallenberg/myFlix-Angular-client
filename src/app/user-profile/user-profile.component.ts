997c71544368320e4b175640a5be9fe7e76694d8
NeleSchallenberg <nschallenberg@gmx.net>
2023-05-20 13:18:42 +0200

    Fix getUser and updateUser function

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
-
-// This import is used to display notifications back to the user
+// import { MatDialogRef } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
-
-// This import brings in the API calls we created in 6.2
 import { FetchApiDataService } from '../fetch-api-data.service';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'app-user-profile',
@@ -12,34 +10,55 @@ import { FetchApiDataService } from '../fetch-api-data.service';
 	styleUrls: ['./user-profile.component.scss'],
 })
 export class UserProfileComponent implements OnInit {
-	@Input() userData = { Username: '', Password: '', Email: '', Birthday: '' };
+	user: any = {};
+	favorites: any[] = [];
+
+	@Input() userData = {
+		Username: '',
+		Password: '',
+		Email: '',
+		Birthday: '',
+	};
 
 	constructor(
 		public fetchApiData: FetchApiDataService,
-
-		public snackBar: MatSnackBar
+		// public dialogRef: MatDialogRef<UserProfileComponent>,
+		public snackBar: MatSnackBar,
+		private router: Router
 	) {}
 
-	ngOnInit(): void {}
+	ngOnInit(): void {
+		this.getUser();
+	}
 
 	// Fetch current user data
 	getUser(): void {
-		this.fetchApiData.getUser().subscribe((response) => {
-			console.log(response);
+		this.fetchApiData.getUser().subscribe((resp: any) => {
+			// we set user variable to keep what we get as a response from API call
+			this.user = resp;
+			// console.log(resp);
+			this.userData.Username = this.user.Username;
+			this.userData.Email = this.user.Email;
+			this.userData.Birthday = this.user.Birthday;
+			return this.user;
+		});
+		this.fetchApiData.getAllMovies().subscribe((resp: any) => {
+			this.favorites = resp.filter(
+				(m: { _id: any }) =>
+					this.user.FavoriteMovies.indexOf(m._id) >= 0
+			);
 		});
 	}
+
 	// This is the function responsible for sending the form inputs to the backend
 	updateUser(): void {
 		this.fetchApiData.editUser(this.userData).subscribe(
 			(response) => {
-				console.log(response);
-				this.snackBar.open(
-					'User updated successfully successfully!',
-					'OK',
-					{
-						duration: 2000,
-					}
-				);
+				localStorage.setItem('user', JSON.stringify(response));
+
+				this.snackBar.open('User updated successfully!', 'OK', {
+					duration: 2000,
+				});
 			},
 			(response) => {
 				console.log(response);
